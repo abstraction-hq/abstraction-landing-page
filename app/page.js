9e3407428699366219eaf969075d260d178be21d
MienPV <mien.pham@vexere.com>
2024-06-08 13:05:42 +0700

    fix bug

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout/Layout"
+import Slider from "@/components/sections/Slider"
+export default function Home() {
+
+    return (
+        <>
+
+            <Layout headerStyle={1} footerStyle={1}>
+                <Slider />
+            </Layout>
+        </>
+    )
+}
