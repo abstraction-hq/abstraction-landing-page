1db31edc4f93f0e98edf9a52d01a8e7e1f8b5fdb
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-16 14:35:35 +0700

    base landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
+  variants: {
+    extend: {
+      textColor: ['dark', 'dark:hover', 'dark:active'],
+    },
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,6 +16,7 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "abstraction-gradient": 'linear-gradient(135deg, #1a2a6c, #b21f1f, #fdbb2d)',
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
